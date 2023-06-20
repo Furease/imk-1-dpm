99a8efba7d4aabc7e6ad9e701d9ac336f4b31dc6
jd <msirojuddin26@gmail.com>
2023-06-20 17:47:26 +0700

    tinggal struktur

--- js/tampilpengumumandiadmin.js
+++ js/tampilpengumumandiadmin.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
-	$(".view").on("click", function () {
-		var src = $(this).data("image");
-		var alt = $(this).data("alt");
+	// $(".view").on("click", function () {
+	// 	var src = $(this).data("image");
+	// 	var alt = $(this).data("alt");
 
-		$(".popup_image").attr("src", src);
-		$(".popup_image").attr("alt", alt);
-	});
-	$("#tampilPengumumanModal").modal("show");
+	// 	$(".popup_image").attr("src", src);
+	// 	$(".popup_image").attr("alt", alt);
+	// });
+	// $("#tampilPengumumanModal").modal("show");
+	$('.collapse-button').click( function () {
+		$(this).children('i').toggleClass('fa-chevron-circle-up fa-chevron-circle-right');
+	  });
 });
